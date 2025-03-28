6f694683b456574e92a860cfa0139fb9b0dc9ddd
LeslieXin92 <lesliexin92@gmail.com>
2025-03-29 03:05:17 +0800

    update: icon mapping

--- constants/IconMapping.ts
+++ constants/IconMapping.ts
@@ -8,14 +8,17 @@ export const IconMapping = {
   'plus': 'add', // 新增账单
   'eye.fill': 'visibility', // 密码显隐-显
   'eye.slash.fill': 'visibility-off', // 密码显隐-隐
-  'xmark.circle.fill': 'cancel', // 清空
+  'xmark.circle.fill': 'cancel', // 清空 & error tips icon
   'sun.max': 'light-mode', // light mode
   'moon': 'dark-mode', // dark mode
   'scribble.variable': 'arrow-right', // 切换账本
   'ellipsis': 'more-horiz', // 更多
   'text.justify': 'menu', // 菜单
   'calendar': 'calendar-month', // 日历
-  'chart.line.uptrend.xyaxis': 'pie-chart-outline' // 图表
+  'chart.line.uptrend.xyaxis': 'pie-chart-outline', // 图表
+  'checkmark.circle.fill': 'check-circle', // success tips icon
+  'exclamationmark.triangle.fill': 'warning', // warning tips icon
+  'info.circle.fill': 'info' // info tips icon
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
