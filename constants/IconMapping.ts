35060e67a4590559e7689e66b694a4f46bb99dd9
LeslieXin92 <lesliexin92@gmail.com>
2025-03-29 01:39:51 +0800

    update: icon mapping

--- constants/IconMapping.ts
+++ constants/IconMapping.ts
@@ -3,15 +3,19 @@ import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 
 // See MaterialIcons here: https://icons.expo.fyi
 export const IconMapping = {
-  'house.fill': 'home',
-  'paperplane.fill': 'send',
-  'chevron.left.forwardslash.chevron.right': 'code',
-  'chevron.right': 'chevron-right',
-  'plus.circle.fill': 'add-circle',
-  'creditcard.fill': 'credit-card',
-  'eye.fill': 'visibility',
-  'eye.slash.fill': 'visibility-off',
-  'xmark.circle.fill': 'cancel'
+  'house.fill': 'home', // 账单tab
+  'creditcard.fill': 'credit-card', // 资产tab
+  'plus': 'add', // 新增账单
+  'eye.fill': 'visibility', // 密码显隐-显
+  'eye.slash.fill': 'visibility-off', // 密码显隐-隐
+  'xmark.circle.fill': 'cancel', // 清空
+  'sun.max': 'light-mode', // light mode
+  'moon': 'dark-mode', // dark mode
+  'scribble.variable': 'arrow-right', // 切换账本
+  'ellipsis': 'more-horiz', // 更多
+  'text.justify': 'menu', // 菜单
+  'calendar': 'calendar-month', // 日历
+  'chart.line.uptrend.xyaxis': 'pie-chart-outline' // 图表
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
